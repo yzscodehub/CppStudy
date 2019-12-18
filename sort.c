356d42db4c192f67a74068962392e13bb89d90c4
yzscodehub <yzs17868813053@outlook.com>
2019-12-18 18:19:15 +0800

    add <7><8>

--- sort.c
+++ sort.c
@@ -135,6 +135,86 @@ void  shellSort(int array[], int lenth)
 
 //归并排序
 
+//堆排序
+
+
+//剑指offer<7>
+//重建二叉树
+struct BinaryTreeNode{
+	int m_nValue;
+	BinaryTreeNode* m_pLeft;
+	BinaryTreeNode* m_pRight;
+};
+BinaryTreeNode* Construct(int* preorder, int* inorder, int length){
+	if(preorder == nullptr || inorder == nullptr || length <= 0){
+		return nullptr;
+	}
+	ConstructCore(preorder, preorder + length - 1, inorder, inorder + length - 1);
+}
+BinaryTreeNode* ConstructCore(int* startPreorder, int* endPreorder, int* startInorder, int* endInorder){
+	//前序遍历序列的第一个数字是根节点的值
+	int rootValue = startPreorder[0];
+	BinaryTreeNode* root = new BinaryTreeNode();
+	root->m_nValue = rootValue;
+	root->m_pLeft = root->m_pRight = nullptr;
+	
+	if(startPreorder == endPreorder){
+		if(startInorder == endInorder && startPreorder == startInorder){
+			return root;
+		}
+		else{
+			throw std::exception("Invalid input.");
+		}
+	}
+	
+	//在中序遍历序列中找到根节点的值
+	int* rootInorder = startInorder;
+	while(rootInorder <= endPreorder && *rootInorder != rootValue){
+		++rootInorder;
+	}
+	if(rootInorder == endInorder && *rootInorder != rootValue){
+		throw std::exception("Invalid input.");
+	}
+	
+	int leftLength = rootInorder - startInorder;
+	int* leftPreorderEnd = startPreorder + leftLength;
+	if(leftLength > 0){
+		root->m_pLeft = ConstructCore(startPreorder + 1, leftPreorderEnd, startInorder, rootInorder - 1);
+	}
+	if(leftLength < endPreorder - startPreorder){
+		root->m_pRight = ConstructCore(leftPreorderEnd + 1, endPreorder, rootInorder + 1, endInorder);
+	}
+	return root;
+}
+
+//二叉树的下一个节点<8>
+BinaryTreeNode* GetNext(BinaryTreeNode* pNode){
+	if(pNode == nullptr){
+		return nullptr;
+	}
+	
+	BinaryTreeNode* pNext = nullptr;
+	if(pNode->m_pRight != nullptr){
+		BinaryTreeNode* pRight = pNode->m_pRight;
+		while(pRight->m_pLeft != nullptr){
+			pRight = pRight->m_pLeft;
+		}
+		pNext = pRight;
+	}
+	else if(pNode->m_pParent != nullptr){
+		BinaryTreeNode* pCurrent = pNode;
+		BinaryTreeNode* pParent = pNode->m_pParent;
+		while(pParent != nullptr && pCurrent == pParent->m_pParent){
+			pCurrent = pParent;
+			pParent = pParent->m_pParent;
+		}
+		pNext = pParent;
+	}
+	return pNext;
+}
+
+
+
 
 
 
