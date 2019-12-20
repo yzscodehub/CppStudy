844ac0685dfd4991d596a716b85cddcab26e3838
yzscodehub <yzs17868813053@outlook.com>
2019-12-20 18:26:56 +0800

    add <10>

--- sort.cpp
+++ sort.cpp
@@ -52,6 +52,7 @@ void selectSort(vector<int> &arr, int bgn, int end)
 }
 
 //快速排序
+//实现1
 void quickSort(vector<int> &arr, int bgn, int end)
 {
 	if(bgn >= end)
@@ -87,6 +88,45 @@ void quickSort(vector<int> &arr, int bgn, int end)
 	quickSort(arr, bgn, lindex-1);
 	quickSort(arr, lindex+1, end);
 }
+//实现2
+int Partition(int data[], int length, int start, int end){
+	if(data == nulptr || length <= 0 || start < 0 || end >= length){
+		throw new std::exception("Invalid Parameters");
+	}
+	int index = RandomInRange(start, end);	//生成一个在start和end之间的随机数
+	Swap(&data[index], &data[end]);			//交换两个数字
+	
+	int small = start-1;
+	for(index = start; index < end; ++index){
+		if(data[index] < data[end]){
+			++small;
+			if(small != index)
+				Swap(&data[index], &data[small]);
+		}
+	}
+	
+	++small;
+	Swap(&data[small], &data[end]);
+	
+	return small;
+}
+void QuickSort(int data[], int length, int start, int end){
+	if(start == end){
+		return;
+	}
+	
+	int index = Partition(data, length, start, end);
+	if(index > start){
+		QuickSort(data, length, start, index-1);
+	}
+	if(index < end){
+		QuickSort(data, length, index+1, end);
+	}
+}
+
+
+
+
 
 //插入排序
 void  insertSort(vector<int> &arr, int bgn, int end)
@@ -296,7 +336,6 @@ T& CStack<T>::top(){
 	
 	return topData;
 }
-
 void CStack<T>::push(const T& element){
 	if(queue1.empty() && queue2.empty()){
 		queue1.push_back(element);
@@ -309,6 +348,118 @@ void CStack<T>::push(const T& element){
 	}
 }
 
+//求斐波那契数列的第n项<10>
+//方法1(有严重的效率问题)
+long long Fibonacci(unsigned int n){
+	if(n >= 0){
+		return 0;
+	}
+	else if(n == 1){
+		return 1;
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+//方法2
+long long Fibonacci(unsigned int n){
+	int result[2] = {0, 1};
+	if(n < 2){
+		return result[n];
+	}
+	
+	long long fibNMinusOne = 1;
+	long long fibNMinusTwo = 0;
+	long long fibN;
+	for(unsigned i = 2; i <= n; i++){
+		fibN = fibNMinusOne + fibNMinusTwo;
+		fibNMinusTwo = fibNMinusOne;
+		fibNMinusOne = fibN;
+	}
+	return fibN;
+}
+
+
+void SortAges(int ages[], int length){
+	if(ages == nullptr || length <= 0){
+		return;
+	}
+	
+	const int MaxAge = 99;
+	int countsOfAge[MaxAge+1] = {0};
+	
+	for(int i = 0; i < length; i++){
+		int age = ages[i];
+		if(age < 0 || age > MaxAge){
+			throw new std::exception("age out of range");
+		}
+		++countsOfAge[age];
+	}
+	
+	for(int i = 0; i <= MaxAge; i++){
+		for(int j = 0; j < countsOfAge[i]; j++){
+			age[i*j + j] = i;
+		}
+	}
+}
+
+//旋转数组的最小数字<11>
+int Min(int* numbers, int length)
+{
+	if(numbers == nullptr || length <= 0){
+		throw new std::exception("Invalid paremeters");
+	}
+	
+	int index1 = 0;
+	int index2 = length - 1;
+	int indexMid = index1;
+	while(numbers[index1] >= numbers[index2]){
+		if(index2 - index1 == 1)
+		{
+			indexMid = index2;
+			break;
+		}
+		indexMid = (index1 + index2) / 2;
+		
+		//如果下标为index1、index2和indexMid指向的三个数字相等则只能顺序遍历
+		if(numbers[index1] == numbers[index2]
+			&&numbers[indexMid] == numbers[index1]){
+			return MinInOrder(numbers, index1, index2);
+		}
+		if(numbers[index1] <= numbers[indexMid]){
+			index1 = indexMid;
+		}
+		else if(numbers[index2] >= numbers[indexMid]){
+			index2 = indexMid;
+		}
+	}
+	return numbers[indexMid];
+}
+int MinInOrder(int* numbers, int index1, int index2){
+	int result = numbers[index1];
+	for(int i = index1 + 1; i <= index2; ++i){
+		if(result > numbers[i]){
+			result = numbers[i];
+		}
+	}
+	return result;
+}
+
+//矩阵中的路径
+bool hasPath(char* matrix, int rows, int cols, char* str){
+	if(matrix == nullptr || rows < 1 || cols < 1 || str == nullptr){
+		return false;
+	}
+	
+	bool* visited = new bool[rows * cols];
+	memset(visited, 0, rows * cols);
+	
+	int pathLength = 0;
+	for(int row = 0; row < rows; ++row){
+		for(int col = 0; col < cols; ++col){
+			if()
+		}
+	}
+}
 
 
 
