58eee84c9b92a3301e77da98ba190994e93e55ee
yzscodehub <yzs17868813053@outlook.com>
2020-01-06 23:04:46 +0800

    updateoffer.cpp

--- the_sword_refers_to_offer.cpp
+++ the_sword_refers_to_offer.cpp
@@ -654,6 +654,134 @@ void InOrderTraversal(BinaryTreeNode* BT){
 
 
 
+//八皇后问题
+bool notDanger(inrt row, int column, int (*chess)[8]){
+	int i, j, flag1 = flag2 = flag3 = flag4 = flag5 = 0;
+	
+	//判断列方向
+	for(i = 0; i < 8; i++){
+		if((*(*chess+i)+j) != 0){
+			flag1 = 1;
+			break;
+		}
+	}
+	
+	//判断左上方
+	for(i = row, j = column; i != 0 && j != 0; i--, j--){
+		if((*(*chess+i)+j) != 0){
+			flag2 = 1;
+			break;
+		}
+	}
+	
+	//判断右上方
+	for(i = row, j = column; i != 0 && j < 8; i--, j++){
+		if((*(*chess+i)+j) != 0){
+			flag3 = 1;
+			break;
+		}
+	}
+	
+	//判断左下方
+	for(i = row, j = column; i < 8 && j != 0 ; i++, j--){
+		if((*(*chess+i)+j) != 0){
+			flag4 = 1;
+			break;
+		}
+	}
+	
+	//判断右下方
+	for(i = row, j = column; i < 8 && j < 8; i++, j++){
+		if((*(*chess+i)+j) != 0){
+			flag5 = 1;
+			break;
+		}
+	}
+	if(flag1 || flag2 || flag3 || flag4 || flag5){
+		return 0;
+	}
+	else{
+		return true;
+	}
+}
+
+/*	
+参数row：表示起始行
+参数column：表示列数
+参数chess: 表示指向棋盘每一行的指针	
+*/
+void EightQueen(int row, int column, int (*chess)[8]){
+	int chess2[8][8], i, j;
+	for(i = 0; i < 8; i++){
+		for(j = 0; j < 8; j++){
+			chess2[i][j] = chess[i][j];
+		}
+	}
+	
+	int count = 0;
+	if(8 == row)
+		printf("第%d种\n", count);
+		for(i = 0; i < 8; i++){
+			for(j = 0; j < 8; j++){
+				printf("%d ", *(*(chess2 + i) + j));
+			}
+			printf("\n");
+		}
+		printf("\n");
+		count++;
+	}
+	else{
+		for(j = 0; j < column; j++){
+			if(notDanger(row, j, chess)){
+				for(i = 0; i < 8; i++){
+					(*(*chess2+row)+i) = 0;
+				}
+				(*(*chess2+row)+j) = 1;
+				
+				EightQueen(row+1, column, chess2);
+			}
+		}
+	}
+}
+
+int main(){
+	int chess[8][8] = {0};
+	EightQueen(0, 8, chess);
+	return 0;
+}
+
+
+//BF算法
+bool BF(const char* str1, const char* str2){
+	if(str1 == NULL || str2 == NULL){
+		return false;
+	}
+	int length = strlen(str1);
+	int length2 = strlen(str2)
+	int i, j;
+	for(i = 0; i < length; i++){
+		if(str[i] == str[j] && j < length2){
+			if( j == length2 - 1)
+			{
+				break;
+			}
+			j++;
+			
+		}
+		else{
+			j = 0;
+		}
+	}
+	if(j = length2){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+//KMP算法
+
+
 
 
 
