22dc228c85fdf6de13022ee0086af0aeed9a9c97
yzscodehub <yzs17868813053@outlook.com>
2020-01-09 09:44:21 +0800

    update 正则表达式匹配

--- the_sword_refers_to_offer.cpp
+++ the_sword_refers_to_offer.cpp
@@ -630,6 +630,38 @@ void DeleteDuplication(ListNode** pHead){
 	}
 }
 
+//正则表达式匹配<19>
+bool match(char* str, char* pattern){
+	if(str == nullptr || pattern == nullptr){
+		return false;
+	}
+	return matchCore(str, pattern);
+}
+bool matchCore(char* str, char* pattern){
+	if(*str == '\0' && *pattern == '\0'){
+		return true;
+	}
+	
+	if(*str != '\0' && *pattern == '\0'){
+		return false;
+	}
+	
+	if(*(pattern+1) == '*'){
+		if(*pattern == *str || (*pattern == '.' && *str != '\0')){
+			return matchCore(str+1, pattern+2)		//移到下一个字符
+				|| matchCore(str+1, pattern)		//匹配串不变，被匹配串移动一个字符
+				|| matchCore(str, pattern+2);		//匹配串移动两个字符
+		}
+		else{
+			return matchCore(str, pattern+2);
+		}
+	}
+	
+	if(*str == *pattern || *pattern == '.' && *str != '\0'){
+		return matchCore(str+1, pattern+1);
+	}
+	return false;
+}
 
 
 //二叉树中序遍历的非递归实现
@@ -783,7 +815,19 @@ bool BF(const char* str1, const char* str2){
 
 
 
-
+//全排列
+void AllArrangement(char arr[], int left, int right){
+	if(left >= right){
+		printf("%s", arr);
+		return;
+	}
+	
+	for(int i = left; i < right; i++){
+		swap(&arr[left], &arr[i]);
+		AllArrangement(arr, left+1, right);
+		swap(&arr[left], &arr[i]);
+	}
+}
 
 
 
